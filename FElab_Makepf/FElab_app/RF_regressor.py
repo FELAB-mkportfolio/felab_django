08b4820e0aa3e90f1c5c590a57e7d465d2ec1c58
2ky1234 <2ky1234@khu.ac.kr>
2021-05-26 18:47:30 +0900

    feature selection company
    feature selection company

--- FElab_Makepf/FElab_app/RF_regressor.py
+++ FElab_Makepf/FElab_app/RF_regressor.py
@@ -3,6 +3,18 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
+import json
+from datetime import date
+import datetime
+from dateutil import rrule
+from calendar import monthrange
+import pymysql
+from dateutil.relativedelta import relativedelta
+import FinanceDataReader as fdr
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
+from sklearn.preprocessing import MinMaxScaler
+
 class RF_model():
     def getDf(self,data):
         X_data = data[data.drop(['Date','Kospi'],axis=1).columns]
@@ -23,3 +35,90 @@ class RF_model():
         for i in corr:
             n_corr.append(abs(i))
         return n_corr
+        
+    def stock_feature(self,select,conn):
+        '''
+        select : '013890'의 형태로 input 필요
+        conn의 경우는 bokdata db 필요
+        '''
+        curs = conn.cursor()
+        # 013890
+        sql = "SELECT * FROM bok_data"
+
+        bok_data = pd.read_sql(sql,conn)
+
+        conn.close()
+        bok_data.rename(columns = {'index':'Date'}, inplace=True)
+        ###
+        # 추후 추가필요사항 -> 정규화 및 shitf 최적화 
+        ###
+        #bok_data # 주의 사항 데이터 마다 현 시점으로 부터 1달전, 2달전 업데이트가 다를 수 있음 NA값이 허용되는 분석 혹은 삭제 필
+
+        ticker = select # input으로 받기 
+        stock_data = fdr.DataReader(ticker, datetime.datetime.now() - relativedelta(years=5), datetime.datetime.now())
+        stock_data = pd.DataFrame(stock_data.resample('MS').first(),columns = ['Close','Change']).reset_index()
+        stock_data = stock_data[stock_data.Close != 0] # 거래정지종목의 Open값이 0으로 나오는 문제 -> row 삭제
+        # 주가가 선반영하는 성질을 반영해, shift를 휴리스틱하게 주었음( 추후 근거 필요 )
+        stock_data['Change'] = stock_data['Change'].shift(1)
+        #rfada_dataset
+        
+        rfada_dataset = pd.merge(stock_data,bok_data,how='left', left_on='Date',right_on='Date')
+        rfada_dataset['KOSPI'] = rfada_dataset['KOSPI'].pct_change()
+        # randomforest 변수선정
+        clf=RandomForestRegressor(n_estimators=1000)
+        X = rfada_dataset[rfada_dataset.columns[3:]].fillna(0)
+        Y = rfada_dataset[rfada_dataset.columns[2]].fillna(0)
+        clf.fit(X,Y)
+        rf_imp = pd.Series(clf.feature_importances_,index=rfada_dataset.columns[3:]).sort_values(ascending=False)
+        rf_imp = pd.DataFrame(rf_imp)
+        rf_imp = rf_imp.rename_axis('feature').reset_index() 
+        predict_rf = clf.predict(X)
+        #print('MSE  : ', mean_squared_error(Y,predict_rf))
+        #print('RMSE : ', np.sqrt(mean_squared_error(Y,predict_rf)))
+        #rint('R^2  : ', r2_score(Y, predict_rf))
+        
+        
+        # adaboost 변수 선정 
+        clf=AdaBoostRegressor(n_estimators=1000)
+        
+        X = rfada_dataset[rfada_dataset.columns[3:]].fillna(0)
+        Y = rfada_dataset[rfada_dataset.columns[2]].fillna(0)
+        clf.fit(X,Y)
+        ada_imp = pd.Series(clf.feature_importances_,index=rfada_dataset.columns[3:]).sort_values(ascending=False)
+        ada_imp = pd.DataFrame(ada_imp)
+        ada_imp = ada_imp.rename_axis('feature').reset_index() 
+        predict_ada = clf.predict(X)
+        #print('MSE  : ', mean_squared_error(Y,predict_ada))
+        #print('RMSE : ', np.sqrt(mean_squared_error(Y,predict_ada)))
+        #print('R^2  : ', r2_score(Y, predict_ada))
+        
+        accuracy =  {'accuracy_measure': ['MSE','RMSE','R^2'],
+            'random_forest': [mean_squared_error(Y,predict_rf),np.sqrt(mean_squared_error(Y,predict_rf)),r2_score(Y, predict_rf)],
+            'ada_boost': [mean_squared_error(Y,predict_ada),np.sqrt(mean_squared_error(Y,predict_ada)),r2_score(Y, predict_ada)]}
+        accuracy = pd.DataFrame(accuracy)
+
+        feature_selection = {
+            'random_forest': [
+                    {
+                    'Feature': list(rf_imp['feature']),
+                    'Importances': list(rf_imp[0])
+                    }
+            ],         
+            'ada_boost': [
+                    {
+                    'Feature': list(ada_imp['feature']),
+                    'Importances': list(ada_imp[0])
+                    }
+            ],
+            'accuracy': [
+                    {
+                    'accuracy_measure': list(accuracy['accuracy_measure']),
+                    'random_forest': list(accuracy['random_forest']),                 
+                    'ada_boost': list(accuracy['ada_boost'])
+                    }
+            ]
+        }  
+
+
+        
+        return feature_selection
